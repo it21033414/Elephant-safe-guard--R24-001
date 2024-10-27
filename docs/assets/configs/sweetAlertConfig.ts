6c5723819c2cf96ad6c927f7ea34b9e69a6a5b55
it21033414 <it21033414@my.sliit.lk>
2024-10-27 23:12:31 +0530

    first commit

---
+++ docs/assets/configs/sweetAlertConfig.ts
@@ -0,0 +1,13 @@
+// sweetAlertConfig config
+export const sweetAlertConfig = {
+  preventDuplicates: true,
+  includeTitleDuplicates: true,
+};
+
+// for testing
+
+// export const sweetAlertConfig = {
+//   preventDuplicates: true,
+//   includeTitleDuplicates: true,
+//   extendedTimeOut: 10000000,
+// }
